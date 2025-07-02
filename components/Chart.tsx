1a8c0e5245d3498848b6be3da0740096175e1ff0
Stef-koyang <101928146+Stef-koyang@users.noreply.github.com>
2025-07-02 11:20:34 +0100

    Chart.tsx

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,19 +1,17 @@
-import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer } from 'recharts';
+'use client';
 
-type Props = {
-  data: any[];
-};
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-export default function Chart({ data }: Props) {
-  return (
-    <ResponsiveContainer width="100%" height={300}>
-      <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="timestamp" />
-        <YAxis />
-        <Tooltip />
-        <Line type="monotone" dataKey="niveau" stroke="#8884d8" strokeWidth={2} />
-      </LineChart>
-    </ResponsiveContainer>
-  );
-}
+const Chart = ({ data }: { data: any[] }) => (
+  <ResponsiveContainer width="100%" height={300}>
+    <LineChart data={data}>
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+      <Line type="monotone" dataKey="niveauEau" stroke="#007bff" strokeWidth={2} />
+    </LineChart>
+  </ResponsiveContainer>
+);
+
+export default Chart;
